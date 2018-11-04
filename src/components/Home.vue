148f7eb34b51ace14ca774a6f0da6ba3653f0a32
Bonifase <orwabonifase@gmail.com>
2018-11-04 15:09:23 +0300

    event listeners

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -15,28 +15,14 @@
 
         <v-card-title primary-title>
           <div>
-            <div class="headline">Name: {{fruit.name}}</div>
-            <span class="grey--text">Price: {{fruit.price}}</span>
+            <div class="headline">{{fruit.name}} <h6 class="grey--text">Price: {{fruit.price}}</h6></div>
           </div>
         </v-card-title>
 
-        <v-card-actions>
-          <v-btn flat>Share</v-btn>
-          <v-btn flat color="purple">Explore</v-btn>
-          <v-spacer></v-spacer>
-          <v-btn icon @click="show = !show">
-            <v-icon>{{ show ? 'keyboard_arrow_down' : 'keyboard_arrow_up' }}</v-icon>
-          </v-btn>
-        </v-card-actions>
-
-        <v-slide-y-transition>
-          <v-card-text v-show="show">
-            I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
-          </v-card-text>
-        </v-slide-y-transition>
       </v-card>
     </v-flex>
   </v-layout>
+  <button v-on:click="buy">Order Now</button>
     </div>
 </template>
 
@@ -55,6 +41,11 @@ export default {name: 'home',
         {name: 'aple', price: 45},
         {name: 'banana', price: 15}
       ]
+    }
+  },
+  methods: {
+    buy: function () {
+      alert('Your order is being processed')
     }
   }
 }
